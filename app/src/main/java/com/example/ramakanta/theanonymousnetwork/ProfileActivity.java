8e8bbc9bb43167fdda4187285298531e82d838e5
rk472 <33664106+rk472@users.noreply.github.com>
2018-01-27 01:47:36 +0530

    data retrived from firebase ...

--- app/src/main/java/com/example/ramakanta/theanonymousnetwork/ProfileActivity.java
+++ app/src/main/java/com/example/ramakanta/theanonymousnetwork/ProfileActivity.java
@@ -144,7 +144,7 @@ public class ProfileActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
+        //drawer initialization
         DrawerLayout drawer =  findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
